bf44bbae2c23519e2fd12124a46585c196ca0a70
XIFDF <2398869479@qq.com>
2019-07-05 17:49:05 +0800

    /

--- src/test/java/com/xifdf/dao/TestJPA.java
+++ src/test/java/com/xifdf/dao/TestJPA.java
@@ -1,15 +1,23 @@
 package com.xifdf.dao;
 
 import com.xifdf.registration_system.RegistrationSystemApplication;
+import com.xifdf.registration_system.controller.SchoolController;
 import com.xifdf.registration_system.dao.ExamDao;
 import com.xifdf.registration_system.dao.LogDao;
 import com.xifdf.registration_system.pojo.Exam;
 import com.xifdf.registration_system.pojo.Log;
+import com.xifdf.registration_system.pojo.School;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.sql.Timestamp;
 import java.util.List;
@@ -19,9 +27,20 @@ import java.util.List;
 @SpringBootTest(classes = RegistrationSystemApplication.class)
 public class TestJPA     {
     @Autowired
-    LogDao logDao;
+    private LogDao logDao;
     @Autowired
-    ExamDao examDao;
+    private ExamDao examDao;
+    @Autowired
+    private SchoolController schoolController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void init(){
+        mockMvc = MockMvcBuilders.standaloneSetup(
+                schoolController).build();
+    }
+
     @Test
     public void testAddLog(){
         Log log = new Log();
@@ -41,5 +60,16 @@ public class TestJPA     {
     public void testGetExam(){
         Exam exam = examDao.queryExamById(2);
         System.out.println(exam.getDateTime());
+    }
+
+    @Test
+    public void testSchoolController() throws Exception{
+        MockHttpServletRequestBuilder requestBuilder =
+                MockMvcRequestBuilders.post("/school/deleteschool");
+        requestBuilder.param("sid", "1");
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        String content = result.getResponse()
+                .getContentAsString();
+        System.out.println(content);
     }
 }
