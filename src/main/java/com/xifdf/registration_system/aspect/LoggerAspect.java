e803203e4be9257cef8b6909c060546bb22a3f09
XIFDF <2398869479@qq.com>
2019-07-05 16:17:34 +0800

    日志操作(删除学校)切面demo

---
+++ src/main/java/com/xifdf/registration_system/aspect/LoggerAspect.java
@@ -0,0 +1,46 @@
+package com.xifdf.registration_system.aspect;
+
+import com.xifdf.registration_system.dao.LogDao;
+import com.xifdf.registration_system.pojo.Log;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Aspect
+@Component
+
+public class LoggerAspect {
+    @Autowired
+    LogDao logDao;
+
+    private static HttpServletRequest getRequest(){
+        return ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+    }
+    private static HttpSession getSession(){
+        return getRequest().getSession();
+    }
+
+    @Pointcut("execution(public * com.xifdf.registration_system.service.*.deleteSchool(..))")
+    public void cut(){}
+
+    @After("cut()")
+    public void deleteSchoolLogger() {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        HttpSession session = LoggerAspect.getSession();
+        Log log = new Log();
+        log.setUserid(Integer.parseInt(session.getAttribute("userid").toString()));
+        log.setType("删除学校");
+        log.setTime(Timestamp.valueOf(df.format(new Date())));
+        logDao.queryAddLog(log);
+    }
+}
